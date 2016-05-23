08bea43247d825d7a9f774cff329efd17c384d0f
l-vincent-l <lara.vincent@gmail.com>
2016-05-23 19:06:27 +0200

    Hide testing_operator and testing_moteur in stats

--- APITaxi_front/documentation/stats.py
+++ APITaxi_front/documentation/stats.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from collections import defaultdict
 from dateutil.relativedelta import relativedelta
 from sqlalchemy import func
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 from flask.ext.security import current_user
 from flask.ext.restful import request
 from ..extensions import user_datastore
@@ -38,6 +38,9 @@ def stats_taxis(dep):
     nb_taxis = []
     nb_active_taxis = []
     tab_nb_taxis = defaultdict(dict)
+    hidden_operator = User.query.filter_by(
+        email=current_app.config.get('HIDDEN_OPERATOR', 'testing_operator')
+    ).first()
 
     if current_user.has_role('admin'):
         nb_taxis = db.session.query(Taxi.added_by,
@@ -97,6 +100,10 @@ def stats_taxis(dep):
                           .join(Taxi.ads) \
                           .filter(ADS.insee.like(depattern)) \
                           .filter(Taxi.last_update_at >= last_day)
+    if hidden_operator:
+        nb_taxis = nb_taxis.filter(Taxi.added_by != hidden_operator.id)
+    if hidden_operator:
+        nb_active_taxis = nb_active_taxis.filter(Taxi.added_by != hidden_operator.id)
 
     tab_nb_taxis['Total']['ntaxis'] = 0
     tab_nb_taxis['Total']['nactivetaxis'] = 0
@@ -109,29 +116,27 @@ def stats_taxis(dep):
 
 
 def list_active_taxis(dep):
+    if not current_user.has_role('admin') and not current_user.has_role('operateur'):
+        return defaultdict(dict)
 
     depattern = '{0:02d}%'.format(dep) if dep else '%'
     last_day = datetime.now() + timedelta(days=-1)
 
     taxis = []
+    hidden_operator = User.query.filter_by(
+        email=current_app.config.get('HIDDEN_OPERATOR', 'testing_operator')
+    ).first()
 
-    if current_user.has_role('admin'):
-        taxis = Taxi.query.join(User).join(Taxi.ads) \
-                .filter(ADS.insee.like(depattern)) \
-                .filter(Taxi.last_update_at >= last_day) \
-                .order_by(Taxi.added_by) \
-                .limit(20) \
-                .all()
+    taxis = Taxi.query.join(User).join(Taxi.ads) \
+            .filter(ADS.insee.like(depattern)) \
+            .filter(Taxi.last_update_at >= last_day)
+    if not current_user.has_role('admin'):
+        taxis = taxis.filter(Taxi.added_by == current_user.id)
 
-    elif current_user.has_role('operateur'):
-        taxis = Taxi.query.join(Taxi.ads) \
-                .filter(ADS.insee.like(depattern)) \
-                .filter(Taxi.last_update_at >= last_day) \
-                .filter(Taxi.added_by == current_user.id) \
-                .limit(20) \
-                .all()
-    else:
-         taxis = []
+    if hidden_operator:
+        taxis = taxis.filter(Taxi.added_by != hidden_operator.id)
+
+    taxis = taxis.order_by(Taxi.added_by).limit(20).all()
 
     tab_taxis = defaultdict(dict)
     for taxi in taxis:
@@ -340,11 +345,29 @@ def stats_hails(dep):
                         .filter(Hail.creation_datetime >= last_year) \
                         .filter(Hail._status == 'accepted_by_customer') \
 
-
+    
     tab_nb_hails['Total']['nhails_d'] = 0
     tab_nb_hails['Total']['nhails_ok_d'] = 0
     tab_nb_hails['Total']['nhails_y'] = 0
     tab_nb_hails['Total']['nhails_ok_y'] = 0
+
+    hidden_operator = User.query.filter_by(
+        email=current_app.config.get('HIDDEN_OPERATOR', 'testing_operator')
+    ).first()
+    hidden_moteur = User.query.filter_by(
+        email=current_app.config.get('HIDDEN_MOTEUR', 'testing_moteur')
+    ).first()
+
+    if hidden_operator:
+        nb_hails_d = nb_hails_d.filter(Hail.operator_id != hidden_operator.id)
+        nb_hails_ok_d = nb_hails_ok_d.filter(Hail.operator_id != hidden_operator.id)
+        nb_hails_y = nb_hails_y.filter(Hail.operator_id != hidden_operator.id)
+        nb_hails_ok_y = nb_hails_ok_y.filter(Hail.operator_id != hidden_operator.id)
+    if hidden_moteur:
+        nb_hails_d = nb_hails_d.filter(Hail.added_by != hidden_moteur.id)
+        nb_hails_ok_d = nb_hails_ok_d.filter(Hail.added_by != hidden_moteur.id)
+        nb_hails_y = nb_hails_y.filter(Hail.added_by != hidden_moteur.id)
+        nb_hails_ok_y = nb_hails_ok_y.filter(Hail.added_by != hidden_moteur.id)
 
     for ha in nb_hails_d:
         tab_nb_hails['Total']['nhails_d'] += ha.nhails
@@ -359,52 +382,39 @@ def stats_hails(dep):
 
 
 def list_hails(dep):
+    if not current_user.has_role('admin') and\
+       not current_user.has_role('operateur') and not current_user.has_role('moteur'):
+        return defaultdict(dict)
 
     depattern = '{0:02d}%'.format(dep) if dep else '%'
     hails = []
 
     last_year = datetime.now() + relativedelta(months=-12)
 
+    hails = db.session.query(Hail.operateur_id, Hail.added_by,
+                             Hail.creation_datetime, Hail.id,
+                             ADS.insee, ADS.numero, Hail._status) \
+                 .join(Hail.taxi_relation) \
+                 .join(Taxi.ads) \
+                 .filter(ADS.insee.like(depattern)) \
+                 .filter(Hail.creation_datetime >= last_year)
 
-    if current_user.has_role('admin'):
-        hails = db.session.query(Hail.operateur_id, Hail.added_by,
-                                 Hail.creation_datetime, Hail.id,
-                                 ADS.insee, ADS.numero, Hail._status) \
-                     .join(Hail.taxi_relation) \
-                     .join(Taxi.ads) \
-                     .filter(ADS.insee.like(depattern)) \
-                     .filter(Hail.creation_datetime >= last_year) \
-                     .order_by(Hail.creation_datetime.desc()) \
-                     .limit(100) \
-                     .all()
-
-    elif current_user.has_role('operateur'):
-        hails = db.session.query(Hail.operateur_id, Hail.added_by,
-                                 Hail.creation_datetime, Hail.id,
-                                 ADS.insee, ADS.numero, Hail._status) \
-                      .join(Hail.taxi_relation) \
-                      .join(Taxi.ads) \
-                      .filter(ADS.insee.like(depattern)) \
-                      .filter(Hail.creation_datetime >= last_year) \
-                      .filter(Hail.operateur_id == current_user.id) \
-                      .order_by(Hail.creation_datetime.desc()) \
-                      .limit(100) \
-                      .all()
-
+    if current_user.has_role('operateur'):
+        hails = hails.filter(Hail.operateur_id == current_user.id)
     elif current_user.has_role('moteur'):
-        hails = db.session.query(Hail.operateur_id, Hail.added_by,
-                                 Hail.creation_datetime, Hail.id,
-                                 ADS.insee, ADS.numero, Hail._status) \
-                      .join(Hail.taxi_relation) \
-                      .join(Taxi.ads) \
-                      .filter(ADS.insee.like(depattern)) \
-                      .filter(Hail.creation_datetime >= last_year) \
-                      .filter(Hail.added_by == current_user.id) \
-                      .order_by(Hail.creation_datetime.desc()) \
-                      .limit(100) \
-                      .all()
-    else:
-        hails = []
+        hails = hails.filter(Hail.added_by == current_user.id)
+    hidden_operator = User.query.filter_by(
+        email=current_app.config.get('HIDDEN_OPERATOR', 'testing_operator')
+    ).first()
+    hidden_moteur = User.query.filter_by(
+        email=current_app.config.get('HIDDEN_MOTEUR', 'testing_moteur')
+    ).first()
+
+    if hidden_operator:
+        hails = hails.filter(Hail.operator_id != hidden_operator.id)
+    if hidden_moteur:
+        hails = hails.filter(Hail.added_by != hidden_moteur.id)
+    hails = hails.order_by(Hail.creation_datetime.desc()).limit(100).all()
 
     tab_hails = defaultdict(dict)
     for hail in hails:
