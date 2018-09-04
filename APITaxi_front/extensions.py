1694b1b8104042d67f753de5b10e06e28c857122
Vincent <lara.vincent@gmail.com>
2018-09-04 14:33:31 +0200

    Update apitaxi_utils & apitaxi_models

--- APITaxi_front/extensions.py
+++ APITaxi_front/extensions.py
@@ -1,4 +1,4 @@
 #coding:utf-8
 
-from APITaxi_utils.cache_user_datastore import CacheUserDatastore
-user_datastore = CacheUserDatastore()
+from APITaxi_utils.custom_user_datastore import CustomUserDatastore
+user_datastore = CustomUserDatastore()
