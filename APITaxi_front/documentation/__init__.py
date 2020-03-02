e304ec3703412bebefe0e5838ba206cbbbbc489c
Julien Castets <castets.j@gmail.com>
2020-03-02 15:57:39 +0100

    Put import statement on top of file

--- APITaxi_front/documentation/__init__.py
+++ APITaxi_front/documentation/__init__.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 
+from . import (
+    examples,
+    index,
+    moteur,
+    operateur,
+    reference,
+    stats,
+)
+
+
 def init_app(app):
-    from . import index
     app.register_blueprint(index.mod)
-    from . import moteur
     app.register_blueprint(moteur.mod)
-    from . import operateur
     app.register_blueprint(operateur.mod)
-    from . import reference
     app.register_blueprint(reference.mod)
-    from . import examples
     app.register_blueprint(examples.mod)
-    from . import stats
     app.register_blueprint(stats.mod)
