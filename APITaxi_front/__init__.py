0ae0a6c136f033cf45a889d04923da5fad94f141
Julien Castets <castets.j@gmail.com>
2020-03-02 15:44:44 +0100

    Refactoring in create_app
    * not possible anymore to override configuration from environment
    (unused feature)
    * PEP8
    * Do not require nor set X-Version header
    * Do not check if "ENV" configuration variable is set

--- APITaxi_front/__init__.py
+++ APITaxi_front/__init__.py
@@ -1,56 +1,48 @@
 # -*- coding: utf-8 -*-
 
+import os
+
+from flask import Flask, request_started, request, request_finished, g
+from flask_bootstrap import Bootstrap
+from flask_uploads import configure_uploads
+
+from APITaxi_models import db
+from APITaxi_utils.login_manager import init_app as init_login_manager
+from APITaxi_models.security import User, Role
+
+from . import backoffice, documentation
+from .backoffice.forms.login import LoginForm
+from .extensions import user_datastore
+
+
 __author__ = 'Vincent Lara'
 __contact__ = "vincent.lara@data.gouv.fr"
 __homepage__ = "https://github.com/"
 __version__ = '0.1.0'
 __doc__ = "Frontend of api.taxi"
 
+
 def create_app():
-    from flask import Flask, request_started, request, request_finished, g
-    from flask_bootstrap import Bootstrap
-    import os
-
     app = Flask(__name__)
-    app.config.from_object('APITaxi_front.default_settings')
-    if 'APITAXI_CONFIG_FILE' in os.environ:
-        app.config.from_envvar('APITAXI_CONFIG_FILE')
-    if not 'ENV' in app.config:
-        app.logger.error('ENV is needed in the configuration')
-        return None
-    if app.config['ENV'] not in ('PROD', 'STAGING', 'DEV'):
-        app.logger.error("""Here are the possible values for conf['ENV']:
-        ('PROD', 'STAGING', 'DEV') your's is: {}""".format(app.config['env']))
-        return None
-    #Load configuration from environment variables
-    for k in list(app.config.keys()):
-        if not k in os.environ:
-            continue
-        app.config[k] = os.environ[k]
 
-    from APITaxi_models import db
+    # Load default configuration
+    app.config.from_object('APITaxi_front.default_settings')
+
+    # Override default conf with environment variable APITAXI_CONFIG_FILE
+    if 'APITAXI_CONFIG_FILE' not in os.environ:
+        raise RuntimeError('APITAXI_CONFIG_FILE environment variable required')
+    app.config.from_envvar('APITAXI_CONFIG_FILE')
+
     db.init_app(app)
-    from . import backoffice
+
     backoffice.init_app(app)
-    from . import documentation
     documentation.init_app(app)
     Bootstrap(app)
 
-    from APITaxi_utils.version import check_version, add_version_header
-    request_started.connect(check_version, app)
-    request_finished.connect(add_version_header, app)
+    configure_uploads(app, (backoffice.extensions.images))
 
-    from flask_uploads import configure_uploads
-    from .backoffice.extensions import images
-    configure_uploads(app, (images))
-    from APITaxi_utils.login_manager import init_app as init_login_manager
-    from .backoffice.forms.login import LoginForm
-
-    from .extensions import user_datastore
     init_login_manager(app, user_datastore, LoginForm)
 
-    from APITaxi_models import security
-    user_datastore.init_app(db, security.User, 
-            security.Role)
+    user_datastore.init_app(db, User, Role)
 
     return app
