970ef279ee6d8a870f8b8f24bf884eeb010d028d
Julien Castets <castets.j@gmail.com>
2020-03-02 15:56:07 +0100

    Put import statement on top of file

--- APITaxi_front/backoffice/__init__.py
+++ APITaxi_front/backoffice/__init__.py
@@ -1,13 +1,23 @@
 # -*- coding: utf-8 -*-
 
+from . import (
+    admin,
+    ads,
+    drivers,
+    hails,
+    home,
+    profile,
+    user_key,
+    zupc,
+)
+
+
 def init_app(app):
-    from APITaxi_models.hail import Hail
-    from . import ads, drivers, home, user_key, zupc, profile, hails, admin
+    app.register_blueprint(admin.blueprint)
     app.register_blueprint(ads.mod)
     app.register_blueprint(drivers.mod)
+    app.register_blueprint(hails.mod)
     app.register_blueprint(home.mod)
+    app.register_blueprint(profile.mod)
     app.register_blueprint(user_key.mod)
     app.register_blueprint(zupc.mod)
-    app.register_blueprint(profile.mod)
-    app.register_blueprint(hails.mod)
-    app.register_blueprint(admin.blueprint)
