324ccb5ebee681fd08a99218641b8aed1d032e96
Julien Castets <castets.j@gmail.com>
2020-03-26 21:26:56 +0100

    Do not require authentication to view documentation

--- APITaxi_front/views/documentation.py
+++ APITaxi_front/views/documentation.py
@@ -8,25 +8,21 @@ blueprint = Blueprint('documentation', __name__)
 
 
 @blueprint.route('/documentation')
-@login_required
 def index():
     return render_template('documentation/index.html')
 
 
 @blueprint.route('/documentation/search')
-@login_required
 def search():
     return render_template('documentation/search.html')
 
 
 @blueprint.route('/documentation/operator')
-@login_required
 def operator():
     return render_template('documentation/operator.html')
 
 
 @blueprint.route('/documentation/reference')
-@login_required
 def reference():
     api_url = current_app.config.get('API_TAXI_URL', '')
     redirect_url = urljoin(api_url, 'doc/')
@@ -34,6 +30,5 @@ def reference():
 
 
 @blueprint.route('/documentation/examples')
-@login_required
 def examples():
     return render_template('documentation/examples.html')
