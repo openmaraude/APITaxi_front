7ef1d1c30820069a63efb7d8c2f38ea61b2e4aaf
Julien Castets <castets.j@gmail.com>
2020-12-11 17:10:47 +0100

    logas: add documentation

--- APITaxi_front/views/generic/logas.py
+++ APITaxi_front/views/generic/logas.py
@@ -16,32 +16,43 @@ class LogAsForm(FlaskForm):
 
 
 class LogAsCookieMixin:
+    """Mixin to manipulate logas cookies.
 
+    logas_secrets is a cookie which stores a list of secrets.
+
+    On log-as, the current user's secret is stored in logas_secrets for later
+    retrieval before login.
+
+    On logout, the secret is retrieved from logas_secrets to login user.
+    """
     cookie_name = 'logas_secrets'
 
     def set_logas_cookie(self, response, logas_secrets):
+        """Store the list of secrets in response cookie."""
         if not logas_secrets:
             response.delete_cookie(self.cookie_name)
         else:
             response.set_cookie(self.cookie_name, json.dumps(logas_secrets))
 
     def load_logas_cookie(self):
+        """Loads the list of secrets from request cookie."""
         value = request.cookies.get(self.cookie_name)
         if not value:
             return []
+        # Value has been crafted and is not JSON: assume it is empty.
         try:
-            keys = json.loads(value)
+            logas_secrets = json.loads(value)
         except json.decoder.JSONDecodeError:
             return []
-
-        if not isinstance(keys, list):
+        # Value is JSON but has been crafted and is not a list: assume it is
+        # empty.
+        if not isinstance(logas_secrets, list):
             return []
-
-        return keys
+        return logas_secrets
 
 
 class LogAsRedirectMixin:
-
+    """Mixin to redirect user when logas or logout is successful."""
     redirect_on_success = None
 
     def get_redirect_on_success(self):
@@ -51,13 +62,15 @@ class LogAsRedirectMixin:
 
 
 class LogAsSQLAUserMixin:
-
     user_model = None
     user_id_attr = 'id'
     user_secret_attr = 'apikey'
 
     def get_users_query(self):
-        """Override to specify the query to limit the users possible to log as."""
+        """List users. Returns <user_model>.query by default, which returns all
+        users.
+
+        To only allow log-as to a subset of users, return a filtered query."""
         if not self.user_model:
             raise NotImplementedError('You should override user_model or get_users_query()')
         return self.user_model.query
@@ -73,6 +86,14 @@ class LogAsView(View, LogAsCookieMixin, LogAsRedirectMixin, LogAsSQLAUserMixin):
     template_name = None
 
     def dispatch_request(self):
+        """For GET requests, return template_name.
+
+        For POST requests:
+
+        - get the User object with the user_id provided
+        - store the current user secret in cookie
+        - login as the User
+        """
         form = LogAsForm()
 
         if form.validate_on_submit():
@@ -97,6 +118,11 @@ class LogAsView(View, LogAsCookieMixin, LogAsRedirectMixin, LogAsSQLAUserMixin):
 class LogoutAsView(View, LogAsCookieMixin, LogAsRedirectMixin, LogAsSQLAUserMixin):
 
     def dispatch_request(self):
+        """If there is no secret stored in cookie, simple logout.
+
+        Otherwise, pop the first secret from the list of secrets stored in
+        cookies, and identify as the user with this secret.
+        """
         form = FlaskForm()
         if not form.validate_on_submit():
             return redirect('/')
@@ -107,7 +133,7 @@ class LogoutAsView(View, LogAsCookieMixin, LogAsRedirectMixin, LogAsSQLAUserMixi
 
         user_filter = {self.user_secret_attr: logas_secrets[0]}
         user = self.get_users_query().filter_by(**user_filter).first()
-        if not user:  # bad API key
+        if not user:
             response = flask_security.views.logout()
         else:
             response = redirect(self.get_redirect_on_success())
