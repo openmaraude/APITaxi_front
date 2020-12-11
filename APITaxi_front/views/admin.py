775e02f14fa93972f3c7d7b0c8ec9ce8313d05cc
Julien Castets <castets.j@gmail.com>
2020-12-11 17:32:53 +0100

    Remove unused cls

--- APITaxi_front/views/admin.py
+++ APITaxi_front/views/admin.py
@@ -4,7 +4,6 @@ from flask import Blueprint, redirect, request, url_for
 from flask_login import login_user
 import flask_security
 from flask_security import login_required, roles_accepted
-from flask_wtf import FlaskForm
 from wtforms import IntegerField
 
 from APITaxi_models2 import User
@@ -20,10 +19,6 @@ blueprint = Blueprint('admin', __name__)
 @roles_accepted('admin')
 def index():
     return redirect(url_for('admin.logas'))
-
-
-class LogAsForm(FlaskForm):
-    user_id = IntegerField()
 
 
 class AdminLogAs(LogAsView):
